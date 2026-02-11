95d02c053490368f3d0dea44a162bc8d0ecd25be
guraiken <gustavo.fiabani@gmail.com>
2026-02-10 21:50:53 -0300

    inicio da aula do Luciano

---
+++ Aulas Luciano/primeiro-projeto-semestre/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./layouts/Header/Header"
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ Aulas Luciano/primeiro-projeto-semestre/src/layouts/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.module.css"
+
+function Header() {
+  return (
+    <header className="header-teste">
+        <h1>Título</h1>
+    </header>
+  )
+}
+
+export default Header
