5e5045fefcd965a39faffa8b07615f6db8bc88b2
guraiken <gustavo.fiabani@gmail.com>
2026-02-11 23:36:35 -0300

    função nova que o alex passou

--- Aulas Alex/revisao-js/src/bibliotecaService.js
+++ Aulas Alex/revisao-js/src/bibliotecaService.js
@@ -21,3 +21,29 @@ export async function insertLivros(titulo, categoria, valor_unitario, estoque_mi
         console.log(rows)
         return rows
 }
+
+export async function livrosMaiorSaidaNoPeriodo(dataInicial, dataFinal) {
+    const [rows] = await pool.query(`SELECT l.id AS livro_id, 
+        l.titulo AS livro, 
+        l.valor_unitario, 
+        m.quantidade_total_saida 
+        FROM livros l 
+        LEFT JOIN 
+        ( SELECT livro_id, SUM(quantidade) AS quantidade_total_saida 
+         FROM movimentacoes 
+         WHERE tipo = 'SAIDA' 
+         AND data_movimentacao 
+         BETWEEN ? AND ? 
+         GROUP BY livro_id ) m ON m.livro_id = l.id 
+         ORDER BY m.quantidade_total_saida DESC`,
+        [dataInicial, dataFinal]);
+    return rows.map((item) => {
+        const quantidade = item.quantidade_total_saida; 
+        const valorUnitario = item.valor_unitario;
+        return { 
+            livro: item.livro, 
+            quantidade_total_saida: quantidade, 
+            valor_total_financeiro_saidas: quantidade * valorUnitario 
+        };
+    });
+} 
