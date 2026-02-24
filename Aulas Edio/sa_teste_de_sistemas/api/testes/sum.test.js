0b5b5302039d28420838b915ef224baf74c92d61
guraiken <gustavo.fiabani@gmail.com>
2026-02-23 22:15:13 -0300

    aula de testes com equal e not equal

--- Aulas Edio/sa_teste_de_sistemas/api/testes/sum.test.js
+++ Aulas Edio/sa_teste_de_sistemas/api/testes/sum.test.js
@@ -16,3 +16,60 @@ describe('the can', () => {
     expect(can.name).toBe('pamplemousse');
   });
 });
+
+const classroom = {
+    studentsQnt: 30,
+    maxRoomCapacity: 40,
+    schoolClass: "Portuguese"
+}
+
+describe('the classroom', () => {
+    test('has 30 students', () => {
+        expect(classroom.studentsQnt).toBe(30)
+    })
+    test('room supports capacity', () =>{
+        expect(classroom.maxRoomCapacity).toBe(40)
+    })
+    test('it´s portuguese', () => {
+        expect(classroom.schoolClass).toBe('Portuguese')
+    })
+})
+
+const fruit01 = {
+    flavor: "grapefruit",
+    ounces: 12
+}
+
+const fruit02 = {
+    flavor: "grapefruit",
+    ounces: 12
+}
+
+
+describe('the la croix cans on my desk', () =>{
+    test('have all the same properties', ()=> {
+        expect(fruit01).toEqual(fruit02)
+    })
+    test('are not the same can', () => {
+        expect(fruit01).not.toEqual(fruit02)
+    })    
+})
+
+const idoso1 = {
+    sabor: 'velho',
+    pesa: 90
+}
+
+const idoso2 = {
+    sabor: 'velho',
+    pesa: 90
+}
+
+describe('o dois velhos', () => {
+    test('os velhos são iguais', () => {
+        expect(idoso1).toEqual(idoso2)
+    })
+    test('os velhos NÃO são iguais', () => {
+        expect(idoso1).not.toEqual(idoso2)
+    })
+})
